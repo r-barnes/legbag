500d29c095e55921d1388aecae62a2270a1e45dd
Richard Barnes <rbarnes@umn.edu>
2017-03-18 17:28:02 -0700

    Simplified code by removing a lot of serial stuff and making the rest dependent on being in a debugging state.

--- leg_bag_bluetooth/leg_bag_bluetooth.ino
+++ leg_bag_bluetooth/leg_bag_bluetooth.ino
@@ -12,6 +12,7 @@
 //                           "DISABLE" or "MODE" or "BLEUART" or
 //                           "HWUART"  or "SPI"  or "MANUAL"
 
+#define DEBUG_MODE                  0
 #define FACTORYRESET_ENABLE         1
 #define MINIMUM_FIRMWARE_VERSION    "0.6.6"
 #define MODE_LED_BEHAVIOUR          "SPI"
@@ -22,8 +23,10 @@ Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_
 
 // A small helper
 void error(const __FlashStringHelper*err) {
-  Serial.println(err);
-  while (1);
+  if(DEBUG_MODE){ //TODO: What to do if there's an error when not in debug mode?
+    Serial.println(err);
+    while (1);
+  }
 }
 
 /**************************************************************************/
@@ -33,53 +36,45 @@ void error(const __FlashStringHelper*err) {
 */
 /**************************************************************************/
 void setup(void){
-  while (!Serial);  // required for Flora & Micro //TODO: Need to remove this in order to get it to start with app
+  if(DEBUG_MODE){
+    while (!Serial);  // required for Flora & Micro //TODO: Need to remove this in order to get it to start with app
+    Serial.begin(115200);
+  }
+
+  //Wait for the board to settle? TODO
   delay(500);
 
   //Set up pin for opening and closing (controlling) the bag
   pinMode(BAG_CONTROL_PIN, OUTPUT);
   digitalWrite(BAG_CONTROL_PIN, LOW);
+  
+  if(!ble.begin(VERBOSE_MODE)) //Initialize the BlueFruit
+    error(F("Error initailizing Bluetooth!"));
 
-  Serial.begin(115200);
-  Serial.println(F("Adafruit Bluefruit Command <-> Data Mode Example"));
-  Serial.println(F("------------------------------------------------"));
-
-  Serial.print(F("Initialising the Bluefruit LE module: "));
-  if ( !ble.begin(VERBOSE_MODE) )
-    error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
-  Serial.println( F("OK!") );
-
-
-  if ( FACTORYRESET_ENABLE ){
-    /* Perform a factory reset to make sure everything is in a known state */
-    Serial.println(F("Performing a factory reset: "));
-    if ( ! ble.factoryReset() ){
+  //Perform a factory reset to make sure everything is in a known state
+  if (FACTORYRESET_ENABLE){
+    if(!ble.factoryReset())
       error(F("Couldn't factory reset"));
-    }
   }
 
   ble.println("AT+GAPDEVNAME=LegBagController");
 
-  if (! ble.waitForOK() )
-    Serial.println(F("Failed to set Bluetooth name."));
+  if (!ble.waitForOK())
+    error(F("Failed to set Bluetooth name."));
 
   ble.echo(false); //Disable command echo from Bluefruit
-  Serial.println("Requesting Bluefruit info:"); //Requesting info
-  ble.info();
+  //ble.info();
   ble.verbose(false);  // debug info is a little annoying after this point!
 
+  //Wait for a BLE connection
   while(!ble.isConnected())
     delay(500);
 
   // LED Activity command is only supported from 0.6.6
-  if ( ble.isVersionAtLeast(MINIMUM_FIRMWARE_VERSION) ){
-    // Change Mode LED Activity
-    Serial.println(F("Change LED activity to " MODE_LED_BEHAVIOUR));
+  if ( ble.isVersionAtLeast(MINIMUM_FIRMWARE_VERSION) )
     ble.sendCommandCheckOK("AT+HWModeLED=" MODE_LED_BEHAVIOUR);
-  }
 
   // Set module to DATA mode
-  Serial.println( F("Switching to DATA mode!") );
   ble.setMode(BLUEFRUIT_MODE_DATA);
 }
 
@@ -89,8 +84,8 @@ void setup(void){
 */
 /**************************************************************************/
 void loop(void){
+/*
   char n, inputs[BUFSIZE+1];
-
   if (Serial.available())
   {
     n = Serial.readBytes(inputs, BUFSIZE);
@@ -101,22 +96,20 @@ void loop(void){
 
     // Send input data to host via Bluefruit
     ble.print(inputs);
-  }
+  }*/
 
-  // Echo received data
-  while ( ble.available() )
-  {
+  while ( ble.available() ){
     int c = ble.read();
 
     if(c=='T'){
       tone(6, 15, 2000);
-      
     } else if(c=='O'){ //Open
       digitalWrite(BAG_CONTROL_PIN, HIGH);
     } else if(c=='C'){ //Close
       digitalWrite(BAG_CONTROL_PIN, LOW);
     }
 
-    Serial.print((char)c);
+    if(DEBUG_MODE)
+      Serial.print((char)c);
   }
 }
