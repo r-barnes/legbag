11559774fa40185efb72f0c6b48ffedcd065daf5
Richard Barnes <rbarnes@umn.edu>
2017-03-18 22:09:29 -0700

    Cleaned up, commented, and simplified code.

--- leg_bag_bluetooth/leg_bag_bluetooth.ino
+++ leg_bag_bluetooth/leg_bag_bluetooth.ino
@@ -1,27 +1,55 @@
+//Leg Bag Control Program
+//By: Richard Barnes (richard.barnes@berkeley.edu, http://rbarnes.org)
+//Use with: Adafruit Feather 32u4 Bluefruit LE
+
+/*
+This program sets up the Feather's Bluetooth with a custom channel name. Once
+connected, it accepts UART serial input via Bluetooth. The program accepts the
+following commands:
+
+    Command | Action
+    O       | Opens the bag by turning the servo to its 0 degree position
+    C       | Closes the bag by turning the servo to its 90 degree position
+*/
+
 #include <Arduino.h>
 #include <SPI.h>
+#include <Servo.h>
 
 #include "Adafruit_BLE.h"
 #include "Adafruit_BluefruitLE_SPI.h"
 #include "Adafruit_BluefruitLE_UART.h"
-
 #include "BluefruitConfig.h"
 
 
-// MODE_LED_BEHAVIOUR        LED activity, valid options are
-//                           "DISABLE" or "MODE" or "BLEUART" or
-//                           "HWUART"  or "SPI"  or "MANUAL"
 
-#define DEBUG_MODE                  0
-#define FACTORYRESET_ENABLE         1
-#define MINIMUM_FIRMWARE_VERSION    "0.6.6"
-#define MODE_LED_BEHAVIOUR          "SPI"
+//Debugging control
+//ENSURE BOTH OF THESE ARE SET TO 0 (ZERO) FOR PRODUCTION
+#define DEBUG_MODE                        0  //Show debugging info in Serial
+#define FACTORYRESET_BLE                  1  //R
 
-#define BAG_CONTROL_PIN             A0
+#define MINIMUM_FIRMWARE_VERSION    "0.6.6"  //Used only to determine what BLE LED can do.
+#define MODE_LED_BEHAVIOUR            "SPI"  //BLE LED behavior. Values: DISABLE, MODE, BLEUART, HWUART, SPI, MANUAL
 
+#define BLE_DEVICE_NAME  "LegBagController"  //Name other Bluetooth devices see when scanning
+
+#define SERVO_CONTROL_PIN                 5  //Pin servo will be controlled by
+#define SERVO_MIN_POS_PW                700  //Pulse width corresponding to minimum position of servo
+#define SERVO_MAX_POS_PW               2300  //Pulse width corresponding to maximum position of servo
+
+
+
+//We'll connect to the servo only when we're using it in order to reduce wear
+//and save energy. The servo's neutral position (90 degrees) is closed for the
+//tube.
+Servo myservo;
+
+//Bluetooth device
 Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
 
-// A small helper
+
+
+//Function for handling errors
 void error(const __FlashStringHelper*err) {
   if(DEBUG_MODE){ //TODO: What to do if there's an error when not in debug mode?
     Serial.println(err);
@@ -29,87 +57,76 @@ void error(const __FlashStringHelper*err) {
   }
 }
 
-/**************************************************************************/
-/*!
-    @brief  Sets up the HW an the BLE module (this function is called
-            automatically on startup)
-*/
-/**************************************************************************/
-void setup(void){
+
+
+//Performs whatever actions are necessary to open the bag
+void OpenBag(){
+  //Connect to the servo and move it to the open position
+  myservo.attach(SERVO_CONTROL_PIN,SERVO_MIN_POS_PW,SERVO_MAX_POS_PW); 
+  myservo.write(0);
+}
+
+
+
+//Performs whatever actions are necessary to close the bag
+void CloseBag(){
+  myservo.write(90);   //Move the servo to its closed position
+  delay(2000);         //Give it 2 seconds to get there, so we don't stop in an intermediate position
+  myservo.detach();    //Disconnect from the servo
+}
+
+
+
+void setup(){
   if(DEBUG_MODE){
-    while (!Serial);  // required for Flora & Micro //TODO: Need to remove this in order to get it to start with app
+    while (!Serial);           //Required for Flora & Micro
     Serial.begin(115200);
   }
 
-  //Wait for the board to settle? TODO
-  delay(500);
+  delay(500);                  //Wait for the board to settle? TODO
 
-  //Set up pin for opening and closing (controlling) the bag
-  pinMode(BAG_CONTROL_PIN, OUTPUT);
-  digitalWrite(BAG_CONTROL_PIN, LOW);
-  
   if(!ble.begin(VERBOSE_MODE)) //Initialize the BlueFruit
     error(F("Error initailizing Bluetooth!"));
 
-  //Perform a factory reset to make sure everything is in a known state
-  if (FACTORYRESET_ENABLE){
-    if(!ble.factoryReset())
+  if (FACTORYRESET_BLE){       //Ensure everything is in a known state
+    if(!ble.factoryReset())    
       error(F("Couldn't factory reset"));
   }
 
-  ble.println("AT+GAPDEVNAME=LegBagController");
+  //Set Bluefruit name, as seen by other Bluetooth devices
+  ble.println("AT+GAPDEVNAME=" BLE_DEVICE_NAME);
 
   if (!ble.waitForOK())
     error(F("Failed to set Bluetooth name."));
 
-  ble.echo(false); //Disable command echo from Bluefruit
-  //ble.info();
-  ble.verbose(false);  // debug info is a little annoying after this point!
+  ble.echo(false);             //Disable command echo from Bluefruit
+  ble.verbose(false);          //Disable debug info from Bluefruit
 
-  //Wait for a BLE connection
-  while(!ble.isConnected())
+  while(!ble.isConnected())    //Wait for a BLE connection
     delay(500);
 
-  // LED Activity command is only supported from 0.6.6
+  //LED Activity command is only supported from 0.6.6 onwards
   if ( ble.isVersionAtLeast(MINIMUM_FIRMWARE_VERSION) )
     ble.sendCommandCheckOK("AT+HWModeLED=" MODE_LED_BEHAVIOUR);
 
-  // Set module to DATA mode
+  //Set module to DATA mode
   ble.setMode(BLUEFRUIT_MODE_DATA);
 }
 
-/**************************************************************************/
-/*!
-    @brief  Constantly poll for new command or response data
-*/
-/**************************************************************************/
-void loop(void){
-/*
-  char n, inputs[BUFSIZE+1];
-  if (Serial.available())
-  {
-    n = Serial.readBytes(inputs, BUFSIZE);
-    inputs[n] = 0;
-    // Send characters to Bluefruit
-    Serial.print("Sending: ");
-    Serial.println(inputs);
 
-    // Send input data to host via Bluefruit
-    ble.print(inputs);
-  }*/
 
+void loop(){
   while ( ble.available() ){
     int c = ble.read();
 
-    if(c=='T'){
-      tone(6, 15, 2000);
-    } else if(c=='O'){ //Open
-      digitalWrite(BAG_CONTROL_PIN, HIGH);
-    } else if(c=='C'){ //Close
-      digitalWrite(BAG_CONTROL_PIN, LOW);
-    }
+    if(c=='O')        //Open
+      OpenBag();
+    else if(c=='C')  //Close
+      CloseBag();
+    
 
     if(DEBUG_MODE)
       Serial.print((char)c);
   }
 }
+
